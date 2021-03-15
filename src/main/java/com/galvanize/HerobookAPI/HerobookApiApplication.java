b9a6ae09168469dff659727c907a76c49dd6445b
joseantoniovegaruiz <joseantonio.vegaruiz@gmail.com>
2021-03-15 18:00:16 -0500

    Init commit

---
+++ src/main/java/com/galvanize/HerobookAPI/HerobookApiApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.HerobookAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerobookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerobookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/HerobookAPI/HerobookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.HerobookAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerobookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
